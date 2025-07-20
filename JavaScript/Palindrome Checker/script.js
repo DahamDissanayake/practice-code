8004acb2a445d6723301bbd583f2977f1bbbc26e
Daham Dissanayake <dahamdissanayake05@gmail.com>
2025-07-20 17:43:37 +0530

    up

--- JavaScript/Palindrome Checker/script.js
+++ JavaScript/Palindrome Checker/script.js
@@ -1,6 +1,22 @@
 const wordin = document.getElementById('wordin')
+const body = document.getElementsByTagName('body')[0]
 
 function check(){
     const value = wordin.value;
-    alert(value)
+    const reversed = value.split('').reverse().join('');
+    console.log(reversed);
+
+    if (value === reversed) {
+        console.log('This is a palindrome');
+        setColor('green')
+    }
+    else{
+        console.log('This is not a palindrome')
+        setColor('red')
+    }   
+}
+
+function setColor(color){
+    body.style.backgroundColor = color;
+
 }
